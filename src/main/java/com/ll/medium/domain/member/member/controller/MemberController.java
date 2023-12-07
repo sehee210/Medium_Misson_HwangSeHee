59160b1974b4d1642286379d3cca8a024f7206de
sehee210 <sehee0210@gmail.com>
2023-12-07 15:55:28 +0900

    Member Controller 생성

---
+++ src/main/java/com/ll/medium/domain/member/member/controller/MemberController.java
@@ -0,0 +1,42 @@
+package com.ll.medium.domain.member.member.controller;
+
+import com.ll.medium.domain.member.member.dto.MemberCreateForm;
+import com.ll.medium.domain.member.member.entity.Member;
+import com.ll.medium.domain.member.member.service.MemberService;
+import jakarta.validation.Valid;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Controller;
+import org.springframework.validation.BindingResult;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@RequiredArgsConstructor
+@Controller
+@RequestMapping("/member")
+public class MemberController {
+
+    private final MemberService memberService;
+
+    @GetMapping("/join")
+    public String signup(MemberCreateForm memberCreateForm) {
+        return "join_form";
+    }
+
+    @PostMapping("/join")
+    public String signup(@Valid MemberCreateForm memberCreateForm, BindingResult bindingResult) {
+        if (bindingResult.hasErrors()) {
+            return "join_form";
+        }
+
+        if (!memberCreateForm.getPassword().equals(memberCreateForm.getPasswordConfirm())) {
+            bindingResult.rejectValue("passwordConfirm", "passwordInCorrect",
+                    "2개의 패스워드가 일치하지 않습니다.");
+            return "join_form";
+        }
+
+        memberService.create(memberCreateForm.getUsername(), memberCreateForm.getPasswordConfirm());
+
+        return "redirect:/";
+    }
+}
