ad6d8d86e41c581d79b1dc9910b1f66085902cba
sehee210 <sehee0210@gmail.com>
2023-12-11 11:00:47 +0900

    로그인 처리를 위한 UserSecurityService 생성

--- src/main/java/com/ll/medium/global/security/SecurityConfig.java
+++ src/main/java/com/ll/medium/global/security/SecurityConfig.java
@@ -2,6 +2,8 @@ package com.ll.medium.global.security;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.config.annotation.authentication.configuration.AuthenticationConfiguration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
@@ -41,5 +43,10 @@ public class SecurityConfig {
     @Bean
     PasswordEncoder passwordEncoder() {
         return new BCryptPasswordEncoder();
+    }
+
+    @Bean
+    AuthenticationManager authenticationManager(AuthenticationConfiguration authenticationConfiguration) throws Exception {
+        return authenticationConfiguration.getAuthenticationManager();
     }
 }
