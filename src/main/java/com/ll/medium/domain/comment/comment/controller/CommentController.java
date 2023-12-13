4b27a85674eec44ba2ead3cbeeffac388799dddf
sehee210 <sehee0210@gmail.com>
2023-12-13 17:38:16 +0900

    postid/comment/commentid/modify URL 맵핑 추가

--- src/main/java/com/ll/medium/domain/comment/comment/controller/CommentController.java
+++ src/main/java/com/ll/medium/domain/comment/comment/controller/CommentController.java
@@ -9,14 +9,13 @@ import com.ll.medium.domain.post.post.entity.Post;
 import com.ll.medium.domain.post.post.service.PostService;
 import jakarta.validation.Valid;
 import lombok.RequiredArgsConstructor;
+import org.springframework.http.HttpStatus;
 import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.validation.BindingResult;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.*;
+import org.springframework.web.server.ResponseStatusException;
 
 import java.security.Principal;
 
@@ -40,5 +39,16 @@ public class CommentController {
         }
         Comment comment = this.commentService.create(post, commentForm.getBody(), member);
         return String.format("redirect:/post/%s#answer_%s", comment.getPost().getId(), comment.getId());
+    }
+
+    @PreAuthorize("isAuthenticated()")
+    @GetMapping("/{postId}/comment/{commentId}/modify")
+    public String commentModify(CommentForm commentForm, @PathVariable("postId") Integer postId, @PathVariable("commentId") Integer commentId, Principal principal) {
+        Comment comment = this.commentService.getComment(commentId);
+        if (!comment.getAuthor().getUsername().equals(principal.getName())) {
+            throw new ResponseStatusException(HttpStatus.BAD_REQUEST, "수정권한이 없습니다.");
+        }
+        commentForm.setBody(comment.getBody());
+        return "domain/comment/comment/comment_form";
     }
 }
